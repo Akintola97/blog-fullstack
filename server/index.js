f51e4d06f8a3538411e445429d09b2ba4cd4034b
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-03-09 12:42:56 -0600

    update

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static('uploads'))
 
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb+srv://blog:Myfullstackblog14@cluster0.hdjwdib.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.mongoose_connect)
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
